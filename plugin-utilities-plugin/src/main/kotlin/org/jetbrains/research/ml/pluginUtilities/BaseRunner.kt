9c0aa760645282f1ced9451d3a01e0f076b07fb5
Anastasiia.Birillo <nbirillo@mail.ru>
2021-07-14 15:45:13 +0300

    Fix ktlint

--- plugin-utilities-plugin/src/main/kotlin/org/jetbrains/research/ml/pluginUtilities/BaseRunner.kt
+++ plugin-utilities-plugin/src/main/kotlin/org/jetbrains/research/ml/pluginUtilities/BaseRunner.kt
@@ -8,8 +8,8 @@ import org.jetbrains.research.ml.pluginUtilities.util.IORunnerArgsParser
  * A simple CLI runner with two arguments: input and output
  * An example of arguments list: -Prunner=cli-runner-example -Pinput=<input path> -Poutput=<output path>
  * */
-object CliRunnerExample : BaseRunner<IORunnerArgs, IORunnerArgsParser>
-    ("cli-runner-example", IORunnerArgsParser) {
+object CliRunnerExample :
+    BaseRunner<IORunnerArgs, IORunnerArgsParser>("cli-runner-example", IORunnerArgsParser) {
 
     override fun run(args: IORunnerArgs) {
         println("Input dir: ${args.inputDir}")
