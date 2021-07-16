9e3479c6d8a67c3d063ba71c990c247bdb65eb1f
Anastasiia.Birillo <nbirillo@mail.ru>
2021-07-16 12:08:17 +0300

    Fix tests: add an ability to process folders in the test data

--- plugin-utilities-core/src/main/kotlin/org/jetbrains/research/pluginUtilities/util/FileUtil.kt
+++ plugin-utilities-core/src/main/kotlin/org/jetbrains/research/pluginUtilities/util/FileUtil.kt
@@ -4,5 +4,6 @@ enum class Extension(val value: String) {
     KT("kt"),
     KTS("kts"),
     PY("py"),
-    TXT("txt")
+    TXT("txt"),
+    EMPTY("")
 }
---
+++ plugin-utilities-test/src/test/resources/org/jetbrains/research/pluginUtilities/util/simpleTests/folders/data/in_1/file_1.kt
@@ -0,0 +1,2 @@
+package org.jetbrains.research.pluginUtilities.util.simpleTests.folders.data.in_1
+
