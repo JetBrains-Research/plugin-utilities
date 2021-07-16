ff4f8ece401a1eb639634d38ef56e2840e7ce512
Furetur <furetur@gmail.com>
2021-07-16 16:40:43 +0500

    refactored into a Parametrized test

--- plugin-utilities-plugin/src/test/kotlin/org/jetbrains/research/pluginUtilities/AndroidSdkPreprocessingTest.kt
+++ plugin-utilities-plugin/src/test/kotlin/org/jetbrains/research/pluginUtilities/AndroidSdkPreprocessingTest.kt
@@ -1,35 +1,46 @@
 package org.jetbrains.research.pluginUtilities
 
+import org.jetbrains.research.pluginUtilities.util.Extension
+import org.jetbrains.research.pluginUtilities.util.ParametrizedBaseTest
 import org.jetbrains.research.pluginUtilities.util.assertDirectoriesEqual
+import org.junit.Assert
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 import java.io.File
 import java.nio.file.Files
 
-const val ANDROID_SDK_PATH = "path/to/my/android/sdk"
+private const val ANDROID_SDK_PATH = "path/to/my/android/sdk"
 
-internal class AndroidSdkPreprocessingTest {
+@RunWith(Parameterized::class)
+class AndroidSdkPreprocessingTest :
+    ParametrizedBaseTest(getResourcesRootPath(::AndroidSdkPreprocessingTest, "java_mock_projects")) {
+
     val preprocessor = Preprocessor(listOf(AndroidSdkPreprocessing(ANDROID_SDK_PATH)))
 
-    private fun runPreprocessing(projectName: String) {
-        val resources = File("src/test/resources/org/jetbrains/research/pluginUtilities/java_mock_projects")
-        val repoDirectory = resources.resolve(projectName)
-        val expectedOutputDirectory = resources.resolve("preprocessed_$projectName")
-        assertCorrectPreprocessing(repoDirectory, expectedOutputDirectory)
+    @JvmField
+    @Parameterized.Parameter(0)
+    var inFolder: File? = null
+
+    @JvmField
+    @Parameterized.Parameter(1)
+    var outFolder: File? = null
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "{index}: ({0}, {1})")
+        fun getTestData() = getInAndOutArray(
+            ::AndroidSdkPreprocessingTest,
+            resourcesRootName = "java_mock_projects",
+            inExtension = Extension.EMPTY,
+            outExtension = Extension.EMPTY
+        )
     }
 
-    private fun assertCorrectPreprocessing(repoDirectory: File, expectedOutputDirectory: File) {
+    @Test
+    fun `should add local properties with path to sdk`() {
         val tempOutputDirectory = Files.createTempDirectory("preprocessed").toFile()
-        preprocessor.preprocess(repoDirectory, tempOutputDirectory)
-        assertDirectoriesEqual(tempOutputDirectory, expectedOutputDirectory)
-    }
-
-    @Test
-    fun `should add local properties to root of SimpleAndroid`() {
-        runPreprocessing("SimpleAndroid")
-    }
-
-    @Test
-    fun `should add local properties to subfolder of EmptyRootSimpleAndroid`() {
-        runPreprocessing("EmptyRootSimpleAndroid")
+        preprocessor.preprocess(inFolder!!, tempOutputDirectory)
+        assertDirectoriesEqual(tempOutputDirectory, outFolder!!)
     }
 }
