07fe3c011806ff35395cbb0a9beb7e342e8ef13a
Anastasiia.Birillo <nbirillo@mail.ru>
2021-07-22 13:58:50 +0300

    Move runners

--- plugin-utilities-core/build.gradle.kts
+++ plugin-utilities-core/build.gradle.kts
@@ -1,2 +1,6 @@
 group = rootProject.group
 version = rootProject.version
+
+dependencies {
+    implementation("com.xenomachina:kotlin-argparser:2.0.7")
+}
--- plugin-utilities-plugin/build.gradle.kts
+++ plugin-utilities-plugin/build.gradle.kts
@@ -2,7 +2,7 @@ group = rootProject.group
 version = rootProject.version
 
 dependencies {
-    implementation("com.xenomachina:kotlin-argparser:2.0.7")
+    implementation(project(":plugin-utilities-core"))
 }
 
 // TODO: move into the project
