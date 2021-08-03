667259fc0afc1a7964faecd595cd3b3ea616f034
Furetur <furetur@gmail.com>
2021-08-03 13:54:45 +0500

    added docs fot getKotlinKavaPreprocessorManager and updated signature

--- plugin-utilities-core/src/main/kotlin/org/jetbrains/research/pluginUtilities/preprocessing/GetPreprocessorManager.kt
+++ plugin-utilities-core/src/main/kotlin/org/jetbrains/research/pluginUtilities/preprocessing/GetPreprocessorManager.kt
@@ -3,9 +3,18 @@ package org.jetbrains.research.pluginUtilities.preprocessing
 import org.jetbrains.research.pluginUtilities.preprocessing.android.AndroidSdkPreprocessor
 import org.jetbrains.research.pluginUtilities.preprocessing.common.DeleteFilesPreprocessor
 
-fun getKotlinJavaPreprocessorManager(androidSdkAbsolutePath: String) = PreprocessorManager(
-    listOf(
-        AndroidSdkPreprocessor(androidSdkAbsolutePath),
-        DeleteFilesPreprocessor(listOf(".idea"))
+const val IDEA_FOLDER_NAME = ".idea"
+
+/**
+ * Creates a PreprocessorManager for Kotlin and Java projects.
+ * Combines [AndroidSdkPreprocessor] and [DeleteFilesPreprocessor],
+ * adding `local.properties` files and deleting `.idea` folders.
+ * @param androidSdkAbsolutePath Absolute path to Android SDK.
+ * If it is null then [AndroidSdkPreprocessor] will not be used
+ */
+fun getKotlinJavaPreprocessorManager(androidSdkAbsolutePath: String?) = PreprocessorManager(
+    listOfNotNull(
+        androidSdkAbsolutePath?.let { AndroidSdkPreprocessor(it) },
+        DeleteFilesPreprocessor(listOf(IDEA_FOLDER_NAME))
     )
 )
