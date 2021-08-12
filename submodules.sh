a56cbb68a3e07278eb8ab56701833f0da1e5d43c
Furetur <furetur@gmail.com>
2021-08-12 12:29:05 +0500

    added a script that clones the submodule

---
+++ submodules.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/JetBrains-Research/plugin-utilies-mock-data \
+  plugin-utilities-plugin/src/test/resources/org/jetbrains/research/pluginUtilities/mock_data
