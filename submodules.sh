0178bed74c1adcb0b425400418e793977aaf9d95
Furetur <furetur@gmail.com>
2021-08-13 19:29:02 +0500

    removed unintentional changes

--- submodules.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-git clone https://github.com/JetBrains-Research/plugin-utilies-mock-data \
-  plugin-utilities-plugin/src/test/resources/org/jetbrains/research/pluginUtilities/mock_data
