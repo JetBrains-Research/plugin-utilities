39f46d6c7613f93fa50cc45f74cdc0bb5603fbc2
Furetur <furetur@gmail.com>
2021-08-18 07:55:48 +0300

    added preprocessing and repository openers

--- examples/examples-plugin/src/main/kotlin/org/jetbrains/research/pluginUtilities/examples/PythonProjectOpenerStarter.kt
+++ examples/examples-plugin/src/main/kotlin/org/jetbrains/research/pluginUtilities/examples/PythonProjectOpenerStarter.kt
@@ -3,6 +3,9 @@ package org.jetbrains.research.pluginUtilities.examples
 import com.intellij.ide.impl.ProjectUtil
 import com.intellij.openapi.application.ApplicationManager
 import com.jetbrains.python.sdk.pythonSdk
+import org.jetbrains.research.pluginUtilities.preprocessing.IDEA_FOLDER_NAME
+import org.jetbrains.research.pluginUtilities.preprocessing.PreprocessorManager
+import org.jetbrains.research.pluginUtilities.preprocessing.common.DeleteFilesPreprocessor
 import org.jetbrains.research.pluginUtilities.runners.BaseRunner
 import org.jetbrains.research.pluginUtilities.runners.IORunnerArgs
 import org.jetbrains.research.pluginUtilities.runners.IORunnerArgsParser
@@ -11,8 +14,17 @@ import org.jetbrains.research.pluginUtilities.sdk.setSdkToProject
 class PythonProjectOpenerStarter :
     BaseRunner<IORunnerArgs, IORunnerArgsParser>("python-project-opener-runner-example", IORunnerArgsParser) {
 
-    // TODO: use preprocessing and implemented functions
+    // Create a preprocessor manager that removes .idea folders from the dataset
+    private val preprocessor = PreprocessorManager(
+        listOf(
+            DeleteFilesPreprocessor(listOf(IDEA_FOLDER_NAME))
+        )
+    )
+
     override fun run(args: IORunnerArgs) {
+        val datasetDir = args.inputDir ?: error("input directory must not be null")
+        preprocessor.preprocessDatasetInplace(datasetDir.toFile())
+        // open projects directly using intellij api because there are no repository openers for Python yet
         getSubdirectories(args.inputDir).forEach { projectPath ->
             ApplicationManager.getApplication().invokeAndWait {
                 ProjectUtil.openOrImport(projectPath, null, true)?.let { project ->
