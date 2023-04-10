b5e1f6e58040289d5a7412636b7d9d2c1dd2912f
anastasia.birillo <anastasia.i.birillo@gmail.com>
2023-04-10 12:51:12 +0200

    Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,7 +82,7 @@ configure(subprojects.filter { it.name != "plugin-utilities-plugin" }) {
             register<MavenPublication>("maven") {
                 groupId = "org.jetbrains.research"
                 artifactId = subprojectName
-                version = "1.1"
+                version = "2.0.2"
                 from(components["java"])
             }
         }
