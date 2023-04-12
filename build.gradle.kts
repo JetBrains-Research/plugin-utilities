23aea94ac1443f986b1069a1d3a577a593c263f0
GirZ0n <ilyavlasov2011@gmail.com>
2023-04-12 19:44:06 +0300

    fix: bump the version to 2.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.util.Properties
 
-val projectVersion = "2.0.4"
+val projectVersion = "2.0.5"
 
 group = "org.jetbrains.research.pluginUtilities"
 version = projectVersion
